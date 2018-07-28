0eb0293bb0f2462d722f486f52395cddb09b1988
Jakob Borg <jakob@kastelo.net>
2018-07-28 23:03:51 +0200

    Refactor HTTP(S) handling (fixes #24)
    This moves event getting into the XGSyncthing library, while also
    refactoring it to use NSURLSession, a delegate for allowing local TLS
    certificates, and proper parameters.
    
    The delegate simply skips HTTPS certificate checking on hosts called
    "localhost", "127.0.0.1", or "::1". I think this is what most people
    would want from this tool.

--- syncthing/XGSyncthing.m
+++ syncthing/XGSyncthing.m
@@ -1,10 +1,15 @@
 #import "XGSyncthing.h"
+#import "LocalhostTLSDelegate.h"
+
+// How long we wait for one event poll
+#define EVENT_TIMEOUT 60.0
 
 @interface XGSyncthing()
 
 @property NSTask *StTask;
 @property (nonatomic, strong) NSXMLParser *configParser;
 @property (nonatomic, strong) NSMutableArray<NSString *> *parsing;
+@property (nonatomic, strong) NSURLSession *session;
 
 @end
 
@@ -17,7 +22,7 @@
 - (bool) runExecutable
 {
     _StTask = [[NSTask alloc] init];
-    
+
     [_StTask setLaunchPath:_Executable];
     [_StTask setArguments:@[@"-no-browser"]];
     [_StTask setQualityOfService:NSQualityOfServiceBackground];
@@ -30,32 +35,50 @@
 {
     if (!_StTask)
         return;
-    
+
     [_StTask interrupt];
     [_StTask waitUntilExit];
 }
 
 - (id)sendRequestToEndpoint:(NSString *)endpoint method:(NSString *)method parameters:(NSDictionary *)parameters
 {
-    NSData *serverData = nil;
-    NSError *myError = nil;
-    NSURLResponse *serverResponse = nil;
-    NSMutableURLRequest *theRequest=[NSMutableURLRequest
-                                     requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@", _URI, endpoint]]
-                                     cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10.0];
-    
+    if (self.session == nil) {
+        // Lazy create a reasonable URLSession
+        NSURLSessionConfiguration *cfg = [NSURLSessionConfiguration defaultSessionConfiguration];
+        self.session = [NSURLSession sessionWithConfiguration:cfg delegate:[[LocalhostTLSDelegate alloc] init] delegateQueue:nil];
+    }
+
+    // Create the URL from base, endpoint and params.
+    NSURLComponents *comps = [NSURLComponents componentsWithString:[NSString stringWithFormat:@"%@%@", _URI, endpoint]];
+    NSMutableArray *queryItems = [NSMutableArray array];
+    for (NSString *key in parameters) {
+        [queryItems addObject:[NSURLQueryItem queryItemWithName:key value:[NSString stringWithFormat:@"%@", parameters[key]]]];
+    }
+    comps.queryItems = queryItems;
+
+    NSMutableURLRequest *theRequest = [NSMutableURLRequest
+                                       requestWithURL:[comps URL]
+                                       cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10.0];
+
     [theRequest setHTTPMethod:method];
     [theRequest setValue:_apiKey forHTTPHeaderField:@"X-API-Key"];
-    
-    serverData = [NSURLConnection sendSynchronousRequest:theRequest
-                                       returningResponse:&serverResponse error:&myError];
-    
-    if (myError)
-        return nil;
-    
-    id json = [NSJSONSerialization JSONObjectWithData:serverData options:
-               NSJSONReadingMutableContainers error:&myError];
-    return json;
+    [theRequest setTimeoutInterval:EVENT_TIMEOUT + 5.0]; // slightly more than the Syncthing event timeout
+
+    __block id result = nil;
+    dispatch_semaphore_t done = dispatch_semaphore_create(0);
+
+    NSURLSessionTask *task = [self.session dataTaskWithRequest:theRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error != nil) {
+            dispatch_semaphore_signal(done);
+            return;
+        }
+        result = [NSJSONSerialization JSONObjectWithData:data options: NSJSONReadingMutableContainers error:&error];
+        dispatch_semaphore_signal(done);
+    }];
+    [task resume];
+
+    dispatch_semaphore_wait(done, DISPATCH_TIME_FOREVER);
+    return result;
 }
 
 - (id)sendGetRequestToEndpoint:(NSString *)endpoint parameters:(NSDictionary *)parameters
@@ -71,7 +94,7 @@
 - (bool)ping
 {
     id json = [self sendGetRequestToEndpoint:@"/rest/system/ping" parameters:nil];
-    
+
 	if ([[json objectForKey:@"ping"] isEqualToString:@"pong"])
 		return true;
 	return false;
@@ -118,6 +141,24 @@
     [self sendPostRequestToEndpoint:@"/rest/db/scan" parameters:nil];
 }
 
+- (id)getEventsSince:(long)since
+{
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    if (since > 0) {
+        // Get events since the last once seen, waiting at most EVENT_TIMEOUT s.
+        [params setValue:[NSNumber numberWithLong:since] forKey:@"since"];
+        [params setValue:[NSNumber numberWithFloat:EVENT_TIMEOUT] forKey:@"timeout"];
+    }
+    else {
+        // Get the last event, whatever it is.
+        [params setValue:[NSNumber numberWithInt:1] forKey:@"limit"];
+    }
+    // TODO(jb): We should specify which events we're interested in,
+    // exactly. Both to avoid the firehose and also to avoid Syncthing
+    // changing stuff underneath our feet in the future.
+    return [self sendGetRequestToEndpoint:@"/rest/events" parameters:params];
+}
+
 - (BOOL)loadConfigurationFromXML
 {
     NSError* error;
@@ -136,7 +177,7 @@
 -(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary<NSString *,NSString *> *)attributeDict {
     [_parsing addObject:elementName];
     NSString *keyPath = [_parsing componentsJoinedByString:@"."];
-    
+
     if ([keyPath isEqualToString:@"configuration.gui"]) {
         if ([[[attributeDict objectForKey:@"tls"] lowercaseString] isEqualToString:@"true"]) {
             _URI = @"https://";
@@ -152,7 +193,7 @@
 
 -(void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string {
     NSString *keyPath = [_parsing componentsJoinedByString:@"."];
-    
+
     if ([keyPath isEqualToString:@"configuration.gui.apikey"]) {
         _apiKey = string;
     } else if  ([keyPath isEqualToString:@"configuration.gui.address"]) {
