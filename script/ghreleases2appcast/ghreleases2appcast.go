b4ee4760b1359df4cb9424c50ac77c49f8cfcfc7
Jerry Jacobs <jerry.jacobs@xor-gate.org>
2019-01-16 10:41:28 +0100

    ghreleases2appcast: Revert incorrect major version mucalculation (see #90)

--- script/ghreleases2appcast/ghreleases2appcast.go
+++ script/ghreleases2appcast/ghreleases2appcast.go
@@ -120,7 +120,7 @@ func githubRepositoryReleaseToSparkleItem(release *github.RepositoryRelease) (sp
 	var sparkleVersion string
 
 	if rSegments[0] > 0 {
-		sparkleVersion = fmt.Sprintf("%03d%02d%02d%02d", rSegments[0] * 100, rSegments[1], rSegments[2], distVersion)
+		sparkleVersion = fmt.Sprintf("%03d%02d%02d%02d", rSegments[0] * 10, rSegments[1], rSegments[2], distVersion)
 	} else {
 		sparkleVersion = fmt.Sprintf("%02d%02d%02d", rSegments[1], rSegments[2], distVersion)
 	}
