fa8221b7fd4cc9186331a7b186e390ef527ab386
Jerry Jacobs <jerry.jacobs@xor-gate.org>
2020-06-03 13:14:28 +0200

    Syncthing resources is downloaded now from zipfile instead of tar

--- syncthing/Scripts/syncthing-resource.sh
+++ syncthing/Scripts/syncthing-resource.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 SYNCTHING_ARCH="amd64"
 SYNCTHING_VERSION="1.6.1"
 SYNCTHING_DIST_URL="https://github.com/syncthing/syncthing/releases/download"
-SYNCTHING_TARBALL_URL="${SYNCTHING_DIST_URL}/v${SYNCTHING_VERSION}/syncthing-macos-${SYNCTHING_ARCH}-v${SYNCTHING_VERSION}.tar.gz"
+SYNCTHING_TARBALL_URL="${SYNCTHING_DIST_URL}/v${SYNCTHING_VERSION}/syncthing-macos-${SYNCTHING_ARCH}-v${SYNCTHING_VERSION}.zip"
 
 CURL_ARGS="--connect-timeout 5 --max-time 10 --retry 5 --retry-delay 3 --retry-max-time 60"
 DL_DIR="${BUILT_PRODUCTS_DIR}/dl"
-SYNCTHING_TARBALL="${DL_DIR}/syncthing-${SYNCTHING_VERSION}.tar.gz"
+SYNCTHING_TARBALL="${DL_DIR}/syncthing-${SYNCTHING_VERSION}.zip"
 APP_RESOURCES_DIR="${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Contents/Resources"
 TAR_DIR="${APP_RESOURCES_DIR}/syncthing"
 
@@ -34,5 +34,5 @@ else
     echo "-- Unpacking syncthing"
     echo "   > ${TAR_DIR}"
     mkdir -p "${TAR_DIR}"
-    tar -xf "${SYNCTHING_TARBALL}" -C "${TAR_DIR}" --strip-components=1
+    tar -xvf "${SYNCTHING_TARBALL}" -C "${TAR_DIR}" --strip-components=1
 fi
