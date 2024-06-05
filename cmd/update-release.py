b6a1c9ff2a79ad85fed7d827232294f91fe0f3ed
Jerry Jacobs <jerry.jacobs@xor-gate.org>
2024-06-05 08:57:17 +0200

    Bump bundled syncthing to v1.27.8

--- cmd/update-release.py
+++ cmd/update-release.py
@@ -14,7 +14,7 @@ import fileinput
 from urllib.request import urlopen
 from string import Template
 
-distVersion   = 2
+distVersion   = 1
 latest_url    = "https://api.github.com/repos/syncthing/syncthing/releases/latest"
 infoPlist     = 'syncthing/Info.plist'
 infoPlistTmpl = 'syncthing/Info.plist.tmpl'
